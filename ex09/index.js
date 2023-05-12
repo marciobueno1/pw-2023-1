ec01aa6ee22221fdd3a5faadcb853e21d5bd9ed3
Márcio Bueno <marcio.bueno@unicap.br>
2023-05-12 19:28:48 -0300

    Adicionado POST e DELETE no Back4App

--- ex09/index.js
+++ ex09/index.js
@@ -1,4 +1,6 @@
 const lista = document.getElementById("lista");
+const inputDescricao = document.getElementById("inputDescricao");
+const btAdd = document.getElementById("btAdd");
 
 const taskUrl = "https://parseapi.back4app.com/classes/Task";
 const headers = {
@@ -10,10 +12,14 @@ const renderizaLista = (lista, tarefas) => {
   lista.innerHTML = "";
   tarefas.forEach((tarefa) => {
     const itemText = document.createTextNode(
-      `${tarefa.description} (${tarefa.done})`
+      `${tarefa.description} (${tarefa.done}) `
     );
+    const button = document.createElement("button");
+    button.innerHTML = "X";
+    button.onclick = () => deleteTask(tarefa.objectId);
     const listItem = document.createElement("li");
     listItem.appendChild(itemText);
+    listItem.appendChild(button);
     lista.appendChild(listItem);
   });
 };
@@ -26,4 +32,50 @@ const getTasks = () => {
     });
 };
 
+const handleBtAddClick = () => {
+  const description = inputDescricao.value;
+  if (!description) {
+    alert("É necessário digitar uma descrição!");
+    return;
+  }
+  btAdd.disabled = true;
+  fetch(taskUrl, {
+    method: "POST",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ description: description }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      getTasks();
+      btAdd.disabled = false;
+      inputDescricao.value = "";
+      inputDescricao.focus();
+      console.log("data", data);
+    })
+    .catch((err) => {
+      btAdd.disabled = false;
+      console.log(err);
+    });
+};
+
+const deleteTask = (id) => {
+  fetch(`${taskUrl}/${id}`, {
+    method: "DELETE",
+    headers: headers,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      getTasks();
+      console.log("data", data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 getTasks();
+
+btAdd.onclick = handleBtAddClick;
