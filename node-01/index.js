6c3bbdf0241d5b864b71240198577fbea1f60abb
Márcio Bueno <marcio.bueno@unicap.br>
2023-05-22 20:04:50 -0300

    Node + Express (3 exemplos básicos)

---
+++ node-01/index.js
@@ -0,0 +1,3 @@
+console.log("Hello World!");
+// console.log(window);
+// console.log(document);
---
+++ node-02/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+let contador = 0;
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/ola", function (req, res) {
+  res.send("Olá Mundo!");
+});
+
+app.get("/contador", function (req, res) {
+  ++contador;
+  res.send({ contador });
+});
+
+app.listen(3000);
